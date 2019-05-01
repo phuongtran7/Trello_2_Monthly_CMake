5b07d90b108cd2b1161f2e371bd285b2c43eae0c
Phuong <phuong.tran@jvncomm.com>
2019-05-01 12:41:26 -0400

    Cannot use std::filesystem for some reason so temporary usse thi to check whether the openSSL is installed.

--- CMake_Trello2Monthly/monthly.cpp
+++ CMake_Trello2Monthly/monthly.cpp
@@ -18,8 +18,8 @@ void monthly::initialize()
 	 * https://github.com/Microsoft/cpprestsdk/issues/929
 	 */
 	// If the user has install OpenSSL
-	if (fs::exists("/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt"))
-	{
+	if (FILE * file = fopen("/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt", "r")) {
+		fclose(file);
 		// TO DO: Look for way to search for cert instead of hard code path
 		http_client_config config;
 		config.set_ssl_context_callback([](boost::asio::ssl::context & context)-> void {
